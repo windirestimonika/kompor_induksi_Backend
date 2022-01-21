2fcbe214ea18a56e34444aa0f4b14ad491249c28
windirestimonika <restimonikawindi@gmail.com>
2022-01-21 16:21:13 +0700

    21/01/2021

--- src/main/java/com/iconpln/kompor_induksi_Backend/service/UserService.java
+++ src/main/java/com/iconpln/kompor_induksi_Backend/service/UserService.java
@@ -1,4 +1,19 @@
 package com.iconpln.kompor_induksi_Backend.service;
 
+import com.iconpln.kompor_induksi_Backend.entity.User;
+import com.iconpln.kompor_induksi_Backend.model.JwtDto;
+import com.iconpln.kompor_induksi_Backend.model.JwtRequest;
+import com.iconpln.kompor_induksi_Backend.model.UserDto;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+
+import java.util.Optional;
+
 public interface UserService {
+    JwtDto login(JwtRequest loginDto) throws DisabledException, BadCredentialsException;
+    void register(UserDto dto);
+    Optional<User> findByUsername(String username);
+    JwtDto changePassword(ChangePasswordDto dto);
+    String getFullNameByUsername(String username);
+    JwtDto generateJwtDto(String username);
 }
