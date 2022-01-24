14b37e3b5bb9a3b58d147c467cbbad842778ce88
Reyhan Alkadri <reyhanalkadri@gmail.com>
2022-01-24 14:40:42 +0700

    update status constant

--- src/main/java/com/iconpln/kompor_induksi_Backend/constant/Status.java
+++ src/main/java/com/iconpln/kompor_induksi_Backend/constant/Status.java
@@ -1,2 +1,23 @@
-package com.iconpln.kompor_induksi_Backend.constant;public class Status {
+package com.iconpln.kompor_induksi_Backend.constant;
+
+public class Status {
+    public static final String STATUS_DRAFT = "draft";
+    public static final String STATUS_BELUM_UNDANG_PENILAI = "belum undang penilai";
+    public static final String STATUS_BELUM_DINILAI = "belum dinilai";
+    public static final String STATUS_DINILAI_SEBAGIAN = "dinilai sebagian";
+    public static final String STATUS_SUDAH_DINILAI = "sudah dinilai";
+    public static final String STATUS_SELESAI = "selesai";
+    //    rekap vendor
+    public static final String STATUS_BELUM_VALIDASI = "belum validasi";
+    public static final String STATUS_SUDAH_VALIDASI = "sudah validasi";
+    public static final String STATUS_KONFIRMASI_PLN_PEDULI = "konfirmasi pln peduli";
+    public static final String STATUS_TIDAK_DISETUJUI = "tidak disetujui";
+    //    status pendanaan pln
+    public static final String STATUS_HIBAH = "hibah";
+    public static final String STATUS_PENDANAAN = "pendanaan";
+    public static final String STATUS_PENDANAAN_TAMBAHAN = "pendanaan tambahan";
+    public static final String STATUS_ELIMINASI = "eliminasi";
+    //    status pengadaan
+    public static final String STATUS_PENGADAAN_ON_PROGRESS = "on progress";
+    public static final String STATUS_PENGADAAN_SELESAI = "selesai";
 }
