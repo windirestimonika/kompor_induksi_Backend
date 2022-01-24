9e3c9a1de2adac6e89ebc2803a90356bbe9b2d57
Reyhan Alkadri <reyhanalkadri@gmail.com>
2022-01-24 14:15:11 +0700

    update suratkeputusanrepo

--- src/main/java/com/iconpln/kompor_induksi_Backend/repository/SuratKeputusanRepo.java
+++ src/main/java/com/iconpln/kompor_induksi_Backend/repository/SuratKeputusanRepo.java
@@ -1,2 +1,8 @@
-package com.iconpln.kompor_induksi_Backend.repository;public interface SuratKeputusanRepo {
+package com.iconpln.kompor_induksi_Backend.repository;
+
+import com.iconpln.kompor_induksi_Backend.entity.SuratKeputusan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface SuratKeputusanRepo extends JpaRepository<SuratKeputusan, String>, JpaSpecificationExecutor<SuratKeputusan> {
 }
