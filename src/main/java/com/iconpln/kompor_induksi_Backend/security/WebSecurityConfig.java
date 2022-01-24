d7d8c2fda93c3a4d206a5d7148e6ea9e13de604d
bkeyunknown <kikizan01@gmail.com>
2022-01-24 07:16:53 +0700

    add WebSecurityConfig

--- src/main/java/com/iconpln/kompor_induksi_Backend/security/WebSecurityConfig.java
+++ src/main/java/com/iconpln/kompor_induksi_Backend/security/WebSecurityConfig.java
@@ -1,4 +1,91 @@
 package com.iconpln.kompor_induksi_Backend.security;
 
-public class WebSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String[] AUTH_WHITELIST = {
+            // -- swagger ui
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui/**",
+            "/webjars/**",
+            "/auth/**"
+    };
+
+//    @Autowired
+//    private JwtAuthEntryPoint authEntryPoint;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+//    @Autowired
+//    private JwtFilter requestFilter;
+
+//    @Autowired
+//    private RestAccessDeniedHandler restAccessDeniedHandler;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().mvcMatchers(AUTH_WHITELIST);
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/auth").permitAll()
+                .antMatchers("/user/register").permitAll()
+                .antMatchers("/dashboard").permitAll()
+                .antMatchers(AUTH_WHITELIST).permitAll()
+                .antMatchers(HttpMethod.GET, "/master_vendor/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/master_user/**").permitAll()
+                .antMatchers("/integrasi/**").permitAll()
+                .antMatchers("/**").authenticated()
+                .and().httpBasic().and()
+//                .exceptionHandling().authenticationEntryPoint(authEntryPoint)
+//                .accessDeniedHandler(restAccessDeniedHandler)
+//                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().headers().frameOptions().disable();
+
+//        httpSecurity.addFilterBefore(requestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 }
