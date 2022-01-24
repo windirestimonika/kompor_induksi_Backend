1a35f95280d4ec0b290fabee2d6d7f0d89035603
Reyhan Alkadri <reyhanalkadri@gmail.com>
2022-01-24 14:13:41 +0700

    update perioderepo

--- src/main/java/com/iconpln/kompor_induksi_Backend/repository/PeriodeRepo.java
+++ src/main/java/com/iconpln/kompor_induksi_Backend/repository/PeriodeRepo.java
@@ -1,2 +1,13 @@
-package com.iconpln.kompor_induksi_Backend.repository;public interface PeriodeRepo {
+package com.iconpln.kompor_induksi_Backend.repository;
+
+import com.iconpln.kompor_induksi_Backend.entity.Periode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PeriodeRepo extends JpaRepository<Periode, String>, JpaSpecificationExecutor<Periode> {
+    List<Periode> findPeriodeByTanggalMulaiAndTanggalSelesaiAndIsActiveTrueAndIsDeletedFalse(LocalDate tanggalMulai, LocalDate tanggalSelesai);
 }
+
