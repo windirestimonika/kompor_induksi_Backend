9a72fda9f9067e6ab91803c35063f1b9c55ef952
Reyhan Alkadri <reyhanalkadri@gmail.com>
2022-01-24 10:47:33 +0700

    add unitrepo jpa

--- src/main/java/com/iconpln/kompor_induksi_Backend/repository/UnitRepo.java
+++ src/main/java/com/iconpln/kompor_induksi_Backend/repository/UnitRepo.java
@@ -1,2 +1,8 @@
-package com.iconpln.kompor_induksi_Backend.repository;public class UnitRepo {
+package com.iconpln.kompor_induksi_Backend.repository;
+
+import com.iconpln.kompor_induksi_Backend.entity.Unit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface UnitRepo extends JpaRepository<Unit, String>, JpaSpecificationExecutor<Unit> {
 }
