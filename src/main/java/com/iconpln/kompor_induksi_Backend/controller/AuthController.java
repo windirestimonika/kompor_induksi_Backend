b2f7d226abae8f593a69412c0bab176f4153dca3
Reyhan Alkadri <reyhanalkadri@gmail.com>
2022-01-24 11:51:00 +0700

    update authcontroller

--- src/main/java/com/iconpln/kompor_induksi_Backend/controller/AuthController.java
+++ src/main/java/com/iconpln/kompor_induksi_Backend/controller/AuthController.java
@@ -1,4 +1,17 @@
 package com.iconpln.kompor_induksi_Backend.controller;
 
+import com.iconpln.kompor_induksi_Backend.entity.User;
+import com.iconpln.kompor_induksi_Backend.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+@PreAuthorize("permitAll()")
 public class AuthController {
+
+    @Autowired
+    private AuthService service;
 }
