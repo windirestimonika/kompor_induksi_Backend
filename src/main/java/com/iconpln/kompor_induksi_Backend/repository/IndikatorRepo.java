1e54d9426724acfc04e4d5ca29f9401046574eb7
Reyhan Alkadri <reyhanalkadri@gmail.com>
2022-01-24 10:44:57 +0700

    add indikatorrepo jpa

--- src/main/java/com/iconpln/kompor_induksi_Backend/repository/IndikatorRepo.java
+++ src/main/java/com/iconpln/kompor_induksi_Backend/repository/IndikatorRepo.java
@@ -1,2 +1,9 @@
-package com.iconpln.kompor_induksi_Backend.repository;public class IndikatorRepo {
+package com.iconpln.kompor_induksi_Backend.repository;
+
+import com.iconpln.kompor_induksi_Backend.entity.Indikator;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface IndikatorRepo extends JpaRepository<Indikator, String>, JpaSpecificationExecutor<Indikator> {
 }
+
