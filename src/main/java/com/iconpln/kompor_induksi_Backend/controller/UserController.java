3fbdd618834f66e7d41e27c4ed0e136241e20f8f
Reyhan Alkadri <reyhanalkadri@gmail.com>
2022-01-24 11:53:08 +0700

    update usercontroller

--- src/main/java/com/iconpln/kompor_induksi_Backend/controller/UserController.java
+++ src/main/java/com/iconpln/kompor_induksi_Backend/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.iconpln.kompor_induksi_Backend.controller;
 
+import com.iconpln.kompor_induksi_Backend.entity.User;
+import com.iconpln.kompor_induksi_Backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserService service;
 }
