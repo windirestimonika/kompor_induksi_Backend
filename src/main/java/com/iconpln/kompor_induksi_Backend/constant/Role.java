dda4ffa10e3c382b8796e322e0a81cb481583588
Reyhan Alkadri <reyhanalkadri@gmail.com>
2022-01-24 14:39:42 +0700

    update role constant

---
+++ src/main/java/com/iconpln/kompor_induksi_Backend/constant/Role.java
@@ -0,0 +1,8 @@
+package com.iconpln.kompor_induksi_Backend.constant;
+
+public class Role {
+    public static final String ROLE_SUPERADMIN = "SUPERADMIN";
+    public static final String ROLE_ADMIN = "ADMIN";
+    public static final String ROLE_PENILAI = "PENILAI";
+    public static final String ROLE_VALIDATOR = "VALIDATOR";
+}
---
+++ src/main/java/com/iconpln/kompor_induksi_Backend/constant/UnitLevel.java
@@ -0,0 +1,2 @@
+package com.iconpln.kompor_induksi_Backend.constant;public class UnitLevel {
+}
