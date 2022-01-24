e715cbb0eb3fc351fa370ea46b24dc02746361c1
bkeyunknown <kikizan01@gmail.com>
2022-01-24 10:20:10 +0700

    update UserData Entity

--- src/main/java/com/iconpln/kompor_induksi_Backend/model/UserData.java
+++ src/main/java/com/iconpln/kompor_induksi_Backend/model/UserData.java
@@ -1,4 +1,21 @@
 package com.iconpln.kompor_induksi_Backend.model;
 
-public class UserData {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserData implements Serializable {
+    private static final long serialVersionUID = -4852628749832491171L;
+    private String username;
+    private String email;
+    private String name;
+    private List<String> modules;
 }
