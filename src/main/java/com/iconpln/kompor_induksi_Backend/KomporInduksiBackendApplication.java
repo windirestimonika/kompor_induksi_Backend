a7187674aa3aaa7b2bf7d0df98e12317765d6fbd
bkeyunknown <kikizan01@gmail.com>
2022-01-24 15:06:46 +0700

    add ComponentScan for assembler | update dependencies

--- src/main/java/com/iconpln/kompor_induksi_Backend/KomporInduksiBackendApplication.java
+++ src/main/java/com/iconpln/kompor_induksi_Backend/KomporInduksiBackendApplication.java
@@ -2,7 +2,9 @@ package com.iconpln.kompor_induksi_Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.iconpln.kompor_induksi_Backend.assembler"})
 @SpringBootApplication
 public class KomporInduksiBackendApplication {
 
