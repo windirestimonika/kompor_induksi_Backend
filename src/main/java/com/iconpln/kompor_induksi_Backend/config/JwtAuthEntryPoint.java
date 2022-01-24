c07fa0f2aa52d67efb94a66171c95d0451f5eafe
bkeyunknown <kikizan01@gmail.com>
2022-01-24 07:44:04 +0700

    update JwtAuthEntryPoint

---
+++ src/main/java/com/iconpln/kompor_induksi_Backend/config/JwtAuthEntryPoint.java
@@ -0,0 +1,19 @@
+package com.iconpln.kompor_induksi_Backend.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint, Serializable {
+    private static final long serialVersionUID = 8057619917480358062L;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
